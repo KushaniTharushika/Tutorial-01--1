c2b9caa30e463495ac5319fa35dc0be47401e404
ASUS <ASUS@KUSHANI>
2024-01-24 14:00:01 +0530

    Add project files.

---
+++ Tutorial 01 -1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_01__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = length * width;
+
+
+            Console.WriteLine("The Area of the rectangle is: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
